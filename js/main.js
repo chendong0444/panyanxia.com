0b61c1102f22b58a0170175455a5b21023a3fa50
chendong0444 <chendong0444@gmail.com>
2013-05-26 17:34:15 +0800

    version 5 Signed-off-by: chendong0444 <chendong0444@gmail.com>

--- js/main.js
+++ js/main.js
@@ -2,9 +2,10 @@ var morePics=function(){
 	$.get("./getPicJson?page="+$("#page").val(),function(data){
 		var tmp='<img src="{0}" alt="{1}" width="285px">';
 		$.each(data,function(i,pic){
-			$("#pics").html($("#pics").html()+tmp.replace('{0}',pic.Url).replace('{1}',pic.Title));
+			$("#pics").append(tmp.replace('{0}',pic.Url).replace('{1}',pic.Title));
 		})
-		$("#page").val(parseInt($("#page").val())+1)
+
+		if(data && data.length>1) $("#page").val(parseInt($("#page").val())+1)
 	},'json')
-	
+	return false;
 }
