0bdb28d83f6eb22d00e2dcabc1dc00f98a9b6e2e
chendong0444 <chendong0444@gmail.com>
2013-06-08 21:29:46 +0800

    gocrawl Signed-off-by: chendong0444 <chendong0444@gmail.com>

--- hello/hello.go
+++ hello/hello.go
@@ -2,10 +2,14 @@ package hello
 
 import (
 	"appengine"
+	"appengine/urlfetch"
 	"appengine/user"
 	"fmt"
 	"html/template"
+	"io/ioutil"
 	"net/http"
+	"regexp"
+	"strings"
 )
 
 func init() {
@@ -32,7 +36,48 @@ func loginHandler(w http.ResponseWriter, r *http.Request) {
 }
 
 func helloHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprint(w, "Hello, world!")
+	c := appengine.NewContext(r)
+	client := urlfetch.Client(c)
+	resp, err := client.Get("http://t66y.com/htm_data/8/1306/917581.html")
+	//resp, err := http.Get("http://www.baidu.com")
+	// resp, err := http.Get("http://www.163.com")
+	if err != nil {
+		fmt.Println("http get error.")
+	}
+	defer resp.Body.Close()
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		fmt.Println("http read error")
+	}
+
+	src := string(body)
+
+	//将HTML标签全转换成小写
+	re, _ := regexp.Compile("\\<[\\S\\s]+?\\>")
+	src = re.ReplaceAllStringFunc(src, strings.ToLower)
+
+	//去除STYLE
+	re, _ = regexp.Compile("\\<style[\\S\\s]+?\\</style\\>")
+	src = re.ReplaceAllString(src, "")
+
+	//去除SCRIPT
+	re, _ = regexp.Compile("\\<script[\\S\\s]+?\\</script\\>")
+	src = re.ReplaceAllString(src, "")
+
+	//去除所有尖括号内的HTML代码，并换成换行符
+	re, _ = regexp.Compile("\\<[\\S\\s]+?\\>")
+	src = re.ReplaceAllString(src, "\n")
+
+	//去除连续的换行符
+	re, _ = regexp.Compile("\\s{2,}")
+	input := re.ReplaceAllString(src, "\n")
+
+	//out := make([]byte, len(input))
+	//out = out[:]
+	//iconv.Convert(input, out, "gb2312", "utf-8")
+
+	fmt.Fprint(w, strings.TrimSpace(input))
+
 }
 
 func rootHandler(w http.ResponseWriter, r *http.Request) {
